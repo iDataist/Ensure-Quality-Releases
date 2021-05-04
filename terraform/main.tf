0d3dd4d946275214ef3d66ba0d19b21c01862d2c
Hui Ren <huiren@Huis-MacBook-Pro.local>
2021-05-04 13:32:46 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ module "network" {
   virtual_network_name = "${var.virtual_network_name}"
   application_type     = "${var.application_type}"
   resource_type        = "NET"
-  resource_group       = "${module.resource_group.resource_group_name}"
+  resource_group       = "${var.resource_group}"
   address_prefix_test  = "${var.address_prefix_test}"
 }
 module "nsg-test" {
@@ -24,7 +24,7 @@ module "nsg-test" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "NSG"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${var.resource_group}"
   subnet_id        = "${module.network.subnet_id_test}"
   address_prefix_test = "${var.address_prefix_test}"
 }
@@ -33,12 +33,12 @@ module "appservice" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "AppService"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${var.resource_group}"
 }
 module "publicip" {
   source           = "./modules/publicip"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "publicip"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${var.resource_group}"
 }
