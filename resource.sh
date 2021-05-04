18a377a4fdcba686130e04dc02c9fc22b3bf1669
Hui Ren <huiren@Huis-MacBook-Pro.local>
2021-05-04 13:04:17 -0500

    Update resource.sh

--- resource.sh
+++ resource.sh
@@ -1,20 +1,80 @@
-uniqueId=20210503
-resourceGroupName="group$uniqueId"
+uniqueid=20210504
+resourcegroup="group$uniqueid"
 location='westus2'
-storageAccountName="blob$uniqueId" 
-containerName='container$uniqueId'
+storageaccount="tfbackend$uniqueid" 
+container="tf-backend-files-$uniqueid"
+keyvault="secrets-kv-$uniqueid"
 
-# Create a resource group
-az group create \
-    --name $resourceGroupName \
-    --location $location
+# # Create the resource group
+# az group create \
+# -n $resourcegroup \
+# -l $location
 
-az storage account create \
-    --name $storageAccountName \
-    --resource-group $resourceGroupName \
-    --location $location
+# # Create the key vault
+# az keyvault create \
+# -n $keyvault \
+# -g $resourcegroup \
+# -l $location
 
-# Create a storage container
-az storage container create \
-    --account-name $storageAccountName \
-    --name $containerName 
+# # Create storage account
+# az storage account create \
+# --resource-group $resourcegroup \
+# --name $storageaccount \
+# --sku Standard_LRS \
+# --encryption-services blob
+
+# # Get storage account key
+# ACCOUNT_KEY=$(az storage account keys list --resource-group $resourcegroup --account-name $storageaccount --query [0].value -o tsv)
+
+# # Create container
+# az storage container create \
+# --name $container \
+# --account-name $storageaccount \
+# --account-key $ACCOUNT_KEY
+
+# # Add the storage account key as a secret in the key vault
+# az keyvault secret set \
+# --vault-name $keyvault \
+# --name "tf-backend-sa-access-key" \
+# --value "$ACCOUNT_KEY"
+
+# Create terraform service principal
+SP=$(az ad sp create-for-rbac)
+# Client ID of the service principal
+CLIENT_ID=$(echo $SP | jq '.appId' | sed 's/"//g')
+# Client secret of the service principal
+CLIENT_SECRET=$(echo $SP | jq '.password' | sed 's/"//g')
+# Set your tenant ID
+TENANT_ID=$(echo $SP | jq '.tenant' | sed 's/"//g')
+# Set your subscription ID
+SUBSCRIPTION=$(az account show --query id --output tsv)
+# Set your subscription ID
+SUBSCRIPTION_NAME=$(az account show --query name --output tsv)
+
+# Add the values as secrets to key vault
+az keyvault secret set --vault-name $keyvault --name "sp-id" --value "$CLIENT_ID"
+az keyvault secret set --vault-name $keyvault --name "sp-secret" --value "$CLIENT_SECRET"
+az keyvault secret set --vault-name $keyvault --name "tenant-id" --value "$TENANT_ID"
+az keyvault secret set --vault-name $keyvault --name "subscription-id" --value "$SUBSCRIPTION"
+
+# Give the SP access to get pipeline secrets
+az role assignment create \
+--assignee $CLIENT_ID \
+--scope "/subscriptions/${SUBSCRIPTION}/resourceGroups/${resourcegroup}/providers/Microsoft.KeyVault/vaults/${keyvault}" \
+--role "reader"
+
+az keyvault set-policy \
+--name $keyvault \
+--spn $CLIENT_ID \
+--subscription $SUBSCRIPTION \
+--secret-permissions get
+
+# Create an DevOps service connection using the above service principal
+az devops service-endpoint azurerm create \
+--azure-rm-service-principal-id $CLIENT_ID \
+--azure-rm-subscription-id $SUBSCRIPTION \
+--azure-rm-subscription-name $SUBSCRIPTION_NAME \
+--azure-rm-tenant-id $TENANT_ID \
+--name dev-sp \
+--organization "https://dev.azure.com/hwdgrmy/" \
+--project "Deploy-VM"
