2b7dbf71992b420fd532f0b6d39888648026ef48
Hui Ren <huiren@Huis-MacBook-Pro.local>
2021-05-08 10:48:47 -0500

    Update ui_tests.py

--- automatedtesting/selenium/ui_tests.py
+++ automatedtesting/selenium/ui_tests.py
@@ -14,42 +14,42 @@ def test_login(driver, user, password):
     driver.find_element_by_id('password').send_keys(password)
     driver.find_element_by_id('login-button').click()
     assert (url+'inventory.html') in driver.current_url, "Login failed"
-    print(f"Login succeeded at {timestamp()}")
+    print("Login succeeded at", timestamp())
 
 def test_add_to_cart(driver, n_products):
-    print(f"#########Test adding {n_products} products to cart#########")
+    print("#########Test adding products to cart#########")
     assert (n_products > 0 & n_products < 7), "There are six products in the inventory"
     products_added = []
     for i in range(n_products):
-        product_url = url + f"inventory-item.html?id={i}"
+        product_url = url + "inventory-item.html?id={}".format(i)
         driver.get(product_url)
         driver.find_element_by_class_name('btn_inventory').click()
         products_added.append(
             driver.find_element_by_class_name('inventory_details_name').text)
-    print(f"Added {', '.join(products_added)} to cart")
+    print("Added: ", ', '.join(products_added))
     driver.get(url + "cart.html")
     products_in_cart = [x.text for x in 
                         driver.find_elements_by_class_name('inventory_item_name')]
     print("The cart has: ", ', '.join(products_in_cart))
     assert set(products_added) <= set(products_in_cart)
-    print(f"Adding products succeeded at {timestamp()}")
+    print("Adding products succeeded at", timestamp())
 
 def test_remove(driver, n_products):
-    print(f"#########Test removing products from the cart#########")
+    print("#########Test removing products from the cart#########")
     driver.get(url + "cart.html")
     products_in_cart = [x.text for x in 
                         driver.find_elements_by_class_name('inventory_item_name')]
-    print(f"Removing {n_products} products from cart")
+    print("Removing {} products from cart".format(n_products))
     n_products_in_cart = len(products_in_cart)
-    assert (n_products <= n_products_in_cart), f"There are {n_products_in_cart} products in cart"
+    assert (n_products <= n_products_in_cart), "There are {} products in cart".format(n_products_in_cart)
     buttons = driver.find_elements_by_class_name('cart_button')
     products_removed = []
     for i in range(n_products):
         buttons[i].click()
         products_removed.append(products_in_cart[i])
-    print(f"Removed: ", ', '.join(products_removed))
+    print("Removed: ", ', '.join(products_removed))
     assert set(products_removed) <= set(products_in_cart)
-    print(f"Removing products succeeded at {timestamp()}")
+    print("Removing products succeeded at", timestamp())
 
 if __name__ == "__main__":
     print ('Starting the browser...')
